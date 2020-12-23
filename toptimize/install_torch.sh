5887f4a8eeebb2bd2018a9c40de0745294a33c4c
Hong-in Lee <theorist17@gmail.com>
2020-12-24 00:28:31 +0900

    ignore pycache

--- toptimize/install_torch.sh
+++ toptimize/install_torch.sh
@@ -1,5 +1,6 @@
-pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 export CUDA=cu101
+# conda install pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch
+pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
 pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.6.0+${CUDA}.html
