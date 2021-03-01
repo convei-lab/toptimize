1ae278e0d4651b1a1ab636f47356761d472d021a
korbjb <korbjb@gmail.com>
2021-03-01 15:59:28 +0900

    commit

--- toptimize/script/Attack/pgd_topo_attack.sh
+++ toptimize/script/Attack/pgd_topo_attack.sh
@@ -11,31 +11,43 @@
 # python attack.py "topo-t10/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 0 -vt 20 -ptb 0.20 -ca -ts 0
 
 # robust model experiment 2
-python attack.py "model-t10/base-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
-python attack.py "model-t10/ours-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 -t 10
-python attack.py "model-t10/base-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
-python attack.py "model-t10/ours-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5 -t 10
-python attack.py "model-t10/base-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
-python attack.py "model-t10/ours-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5 -t 10
-python attack.py "model-t10/base-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
-python attack.py "model-t10/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -t 10
+# python attack.py "model-t10/base-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
+# python attack.py "model-t10/ours-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 -t 10
+# python attack.py "model-t10/base-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
+# python attack.py "model-t10/ours-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5 -t 10
+# python attack.py "model-t10/base-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
+# python attack.py "model-t10/ours-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5 -t 10
+# python attack.py "model-t10/base-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
+# python attack.py "model-t10/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -t 10
 
 # robust model experiment
-python attack.py "model-t8/base-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
-python attack.py "model-t8/ours-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 -t 8
-python attack.py "model-t8/base-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
-python attack.py "model-t8/ours-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5 -t 8
-python attack.py "model-t8/base-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
-python attack.py "model-t8/ours-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5 -t 8
-python attack.py "model-t8/base-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
-python attack.py "model-t8/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -t 8
+# python attack.py "model-t8/base-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
+# python attack.py "model-t8/ours-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 -t 8
+# python attack.py "model-t8/base-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
+# python attack.py "model-t8/ours-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5 -t 8
+# python attack.py "model-t8/base-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
+# python attack.py "model-t8/ours-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5 -t 8
+# python attack.py "model-t8/base-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
+# python attack.py "model-t8/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -t 8
 
 # robust model experiment
-python attack.py "model-t12/base-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
-python attack.py "model-t12/ours-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 -t 12
-python attack.py "model-t12/base-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
-python attack.py "model-t12/ours-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5 -t 12
-python attack.py "model-t12/base-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
-python attack.py "model-t12/ours-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5 -t 12
-python attack.py "model-t12/base-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
-python attack.py "model-t12/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -t 12
+# python attack.py "model-t12/base-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
+# python attack.py "model-t12/ours-ptb5"  pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 -t 12
+# python attack.py "model-t12/base-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
+# python attack.py "model-t12/ours-ptb10" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5 -t 12
+# python attack.py "model-t12/base-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
+# python attack.py "model-t12/ours-ptb15" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5 -t 12
+# python attack.py "model-t12/base-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
+# python attack.py "model-t12/ours-ptb20" pgd_attack dev-7_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -t 12
+
+# robust model experiment
+# python attack.py "model-8/base-ptb5"  pgd_attack dev-8_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.05 -ca -ts 0      
+# python attack.py "model-8/ours-ptb5"  pgd_attack dev-8_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.05 -ca -ts 5 
+# python attack.py "model-8/base-ptb10" pgd_attack dev-8_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.1  -ca -ts 0      
+# python attack.py "model-8/ours-ptb10" pgd_attack dev-8_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.1  -ca -ts 5
+# python attack.py "model-8/base-ptb15" pgd_attack dev-8_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.15 -ca -ts 0      
+# python attack.py "model-8/ours-ptb15" pgd_attack dev-8_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.15 -ca -ts 5
+# python attack.py "model-8/base-ptb20" pgd_attack dev-8_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0
+# python attack.py "model-8/ours-ptb20" pgd_attack dev-8_Cora_GCN -vr 19 -vm 20 -vt 0 -ptb 0.20 -ca -ts 5 -ea # t, ea, l1, hs, l2, lr, ts
+# python attack.py "model-test/base-ptb20" pgd_attack dev-8_Cora_GCN -vr 19 -vm  0 -vt 0 -ptb 0.20 -ca -ts 0      
+python attack.py "model-test/ours-ptb20" pgd_attack dev-8_Cora_GCN -vr 19 -vm 10  -vt 0 -ptb 0.20 -ca -ts 5 -ea -t 12
